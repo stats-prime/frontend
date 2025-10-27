80ef61d00f7bb8d2ef7e54507f7988721aa64629
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-26 20:27:38 -0500

    Configuracion axiosClient

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_URL}`,
-    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
